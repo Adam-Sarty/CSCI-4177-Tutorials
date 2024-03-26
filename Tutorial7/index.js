519371c831e3530775c10fe861477aa6fe18613e
Adam Sarty <ad883184@dal.ca>
2024-03-25 22:09:09 -0300

    Update

--- Tutorial7/index.js
+++ Tutorial7/index.js
@@ -6,7 +6,7 @@
  */
 
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 app.use(express.json());
